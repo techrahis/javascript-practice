2b4cec0908c10c507cc1f175f4a202f098968015
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-11 13:27:43 +0530

    Object started

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,8 @@
+const introduceMyself = {
+name: "Rajarshi Samaddar",
+email: "hello@rajarshisamaddar.com",
+myMotto: “Trust the process and enjoy the journey”,
+background: “B.Tech Computer Science and Engineering”,
+status: “Looking for full-time MERN stack opportunities”,
+happyToConnect: true
+}
