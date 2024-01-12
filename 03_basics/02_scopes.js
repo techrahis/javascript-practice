103cca5414bb4f01201ce206b6d92fce8279fc0d
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-12 12:50:16 +0530

    more depth in scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,13 +7,41 @@ if (true) {
   var b = 6;
   const c = 6;
 
-  console.log("Block");
-  console.log(a);
-  console.log(b);
-  console.log(c);
+  //   console.log("Block");
+  //   console.log(a);
+  //   console.log(b);
+  //   console.log(c);
 }
 
-console.log("Global");
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log("Global");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// +++++++++++++++++ Closure +++++++++++++++++
+function x() {
+  const user = "raj";
+  function y() {
+    const user2 = "ram";
+    console.log(user); // using the parent function object
+  }
+  //   console.log(user2); // error
+  y();
+}
+x();
+
+// +++++++++++++++++ Hoisting +++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num) {
+  return num + 1;
+}
+
+// console.log(addOne2(5)); //error
+
+const addOne2 = function (num) {
+  return num + 1;
+};
+
+console.log(addOne2(5)); // coorect
