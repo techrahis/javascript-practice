139c936f689a239165e2b518e9f6dc29711e0774
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-20 12:29:07 +0530

    feat: while and do-while loop complete 🚀

---
+++ 05_iteration/02_while_and_do_while.js
@@ -0,0 +1,23 @@
+const students = ["raj", "rahul", "amit", "rakesh"];
+
+// while loop
+
+let i = 0;
+while (students.length > 0 && i < students.length) {
+  console.log(students[i]);
+  i++;
+}
+
+// do while loop
+
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 10);
+
+let x = 11;
+do {
+  console.log(x);
+  x++;
+} while (x < 10);
