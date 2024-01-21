f685e67a341ded353770413d65516d2e915c0bb5
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-21 15:57:12 +0530

    refactor: what is call-back function

--- 05_iteration/05_for_each.js
+++ 05_iteration/05_for_each.js
@@ -12,6 +12,10 @@ const codingLanguages = [
 ];
 
 // for each loop - used in Arrays
+// it uses a callback function : a function that is passed as an argument to another function
+// the callback function is executed inside the other function
+// the callback function is executed once for each element in the array
+// the callback function takes 3 arguments: the current element, the index of the current element, and the array itself
 
 codingLanguages.forEach((language) => {
   console.log(language);
