4d13fe38caeccdbb605eb5947aadc9af3ad343d3
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-21 15:25:23 +0530

    refactor: practice previous concepts

--- practice/01.js
+++ practice/01.js
@@ -13,3 +13,30 @@ const citizen = {
 };
 
 checkEligibility(citizen);
+
+// concept of reference variables in JavaScript.
+const temp = [1, 2, 3, 4, 5];
+const temp2 = temp;
+temp2.pop();
+console.log(temp2);
+console.log(temp);
+
+let date = new Date();
+console.log(date.toLocaleDateString((location = "en-IN")));
+console.log(date.toLocaleDateString((location = "bn-IN")));
+
+const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+const val = 2;
+
+const removeElement = (nums, val) => {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+};
+
+removeElement(nums, val);
+console.log(nums);
+
+
